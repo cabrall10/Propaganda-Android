78f528f5954730f32a1968f45ab97ce0d795777c
cabrall10 <jeffersoncabral_10@hotmail.com>
2019-11-18 15:45:16 -0300

    Remove variáveis n utilizadas

--- app/src/main/java/com/jefferson/propaganda/MainActivity.java
+++ app/src/main/java/com/jefferson/propaganda/MainActivity.java
@@ -23,8 +23,6 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView myadd;
     private ViewFlipper viewFlipper;
-    static AsyncTask task;
-    private String url = "https://gist.githubusercontent.com/cabrall10/0db36b0d5cb0134d0e703a053a092a0e/raw/877c1df95fc0d64830328a620e9d835c2a917f4c/propagandas.json";
 
     @SuppressLint("StaticFieldLeak")
     @Override
@@ -36,29 +34,29 @@ public class MainActivity extends AppCompatActivity {
 			.build();
         ImageLoader.getInstance().init(config);
 
+        //banner de texto
         myadd = (TextView)this.findViewById(R.id.ads);
         myadd.setSelected(true);
 
         configurarViewFlipper();
 
+        AsyncViewFliperActivity viewFliperActivity = new AsyncViewFliperActivity() {
+            @Override
+            protected void onPostExecute(Object s) {
+                super.onPostExecute(s);
+                try {
 
-            AsyncViewFliperActivity viewFliperActivity = new AsyncViewFliperActivity() {
-                @Override
-                protected void onPostExecute(Object s) {
-                    super.onPostExecute(s);
-                    try {
-
-                        JSONArray array = new JSONArray(s.toString());
-                        for (int i = 0; i < array.length(); i++) {
-                            if (array.getString(i) != null)
-                                preencherViewFlipper(array.getString(i));
-                        }
-                    } catch (Exception e) {
-                        Log.d("error", e.getMessage());
+                    JSONArray array = new JSONArray(s.toString());
+                    for (int i = 0; i < array.length(); i++) {
+                        if (array.getString(i) != null)
+                            preencherViewFlipper(array.getString(i));
                     }
+                } catch (Exception e) {
+                    Log.d("error", e.getMessage());
                 }
-            };
-            viewFliperActivity.execute();
+            }
+        };
+        viewFliperActivity.execute();
     }
 
     public void preencherViewFlipper(String image){
